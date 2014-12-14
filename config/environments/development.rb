c2141ddc420f6e9ffe52dec0686bd51157c55efc
saragiberson <saragiber@gmail.com>
2014-12-14 12:13:25 -0500

    generated action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,6 @@ Rails.application.configure do
   config.assets.raise_production_errors = true
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -37,4 +35,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.perform_deliveries = true 
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'financialmetricwatch',
+    password:             'flatironfis',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
