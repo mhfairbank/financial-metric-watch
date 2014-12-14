86ac422d50bad80c3b4328bb085a999e43560163
mhfairbank <mhfairbank@gmail.com>
2014-12-14 18:48:17 -0500

    build out restful routing and build backend for listener form

--- app/controllers/listeners_controller.rb
+++ app/controllers/listeners_controller.rb
@@ -1,4 +1,10 @@
 class ListenersController < ApplicationController
   def create
+    @listener = Listener.create()
+    @listener.user = current_user
+    @listener.company_id = params[:listener][:company_id]
+    @listener.trigger = params[:listener][:trigger]
+    @listener.save
+    redirect_to :back
   end
 end
