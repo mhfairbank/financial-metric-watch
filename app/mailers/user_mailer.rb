f02254ae095d3b8815cab20497ba750d3843dbc6
mhfairbank <mhfairbank@gmail.com>
2014-12-14 13:23:14 -0500

    add more mailer stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,16 @@
 class UserMailer < ActionMailer::Base
   default from: "financialmetricwatch@gmail.com"
+
+  def test_email
+    mail(to: "mhfairbank@gmail.com", subject: "Financial Metric Test", reply_to: "financialmetricwatch@gmail.com")
+  end
+
+  def trigger_email(user, listener)
+    @user = user
+    @listener = listener
+    
+    mail(to: @user.email, subject: "Listener Triggered", reply_to: "financialmetricwatch@gmail.com")
+  end
+
+
 end
