b2525b8223b58411fd9d5830e8d3b072d3fc1eab
mhfairbank <mhfairbank@gmail.com>
2014-12-14 19:33:36 -0500

    too tired to explain

--- app/assets/javascripts/sectors.js
+++ app/assets/javascripts/sectors.js
@@ -25,49 +25,49 @@ $(function() {
 
   });
 
-  $("#1").on("click", function() {
-    $("#2, #3, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#2").on("click", function() {
-    $("#1, #3, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#3").on("click", function() {
-    $("#1, #2, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#4").on("click", function() {
-    $("#2, #3, #1, #5, #6, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#5").on("click", function() {
-    $("#2, #3, #4, #1, #6, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#6").on("click", function() {
-    $("#2, #3, #4, #5, #1, #7, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#7").on("click", function() {
-    $("#2, #3, #4, #5, #6, #1, #8, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#8").on("click", function() {
-    $("#2, #3, #4, #5, #6, #7, #1, #9, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#9").on("click", function() {
-    $("#2, #3, #4, #5, #6, #7, #8, #1, #10, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#10").on("click", function() {
-    $("#2, #3, #4, #5, #6, #7, #8, #9, #1, #11").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
-  $("#11").on("click", function() {
-    $("#2, #3, #4, #5, #6, #7, #8, #9, #10, #1").slideUp();
-    $(this).parents(".col-md-4").attr("class", "col-md-12");
-  });
+  // $("#1").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#2").on("click", function() {
+  //   $("#1, #3, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#3").on("click", function() {
+  //   $("#1, #2, #4, #5, #6, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#4").on("click", function() {
+  //   $("#2, #3, #1, #5, #6, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#5").on("click", function() {
+  //   $("#2, #3, #4, #1, #6, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#6").on("click", function() {
+  //   $("#2, #3, #4, #5, #1, #7, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#7").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #1, #8, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#8").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #7, #1, #9, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#9").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #7, #8, #1, #10, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#10").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #7, #8, #9, #1, #11").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
+  // $("#11").on("click", function() {
+  //   $("#2, #3, #4, #5, #6, #7, #8, #9, #10, #1").slideUp();
+  //   $(this).parents(".col-md-4").attr("class", "col-md-12");
+  // });
 });
 
